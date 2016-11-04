415749591a651539e76cdaa07b71e3cf4678e3e5
rubenp9 <ruben28693@gmail.com>
2016-11-04 20:45:28 +0100

    Create b00.cpp

---
+++ b00.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numCasos = 0;
+
+	cin >> numCasos;
+
+	for (; numCasos > 0; --numCasos)
+	{
+		int numDecimos = 0;
+
+		cin >> numDecimos;
+		int decimo = 0, cont = 0;
+
+		for (; numDecimos > 0; --numDecimos)
+		{
+			cin >> decimo;
+			if (decimo % 2 == 0){
+				++cont;
+			}
+		}
+
+		cout << cont;
+	}
+
+	return 0;
+}
